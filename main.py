2982f357f64e7ba7abb0e840a196698d0fc6f2f2
Gustavo Pereira Goncalves de Campos <gpereira@sg.unicamp.br>
2025-08-21 13:31:04 -0300

    feat: add except for functions

--- main.py
+++ main.py
@@ -28,182 +28,209 @@ emprestimos = []
 #----------------------------
 
 def gerar_id (nome_arquivo):
-    if not os.path.exists(nome_arquivo) or os.path.getsize(nome_arquivo) == 0:
-        return 1
-    with open (nome_arquivo, "r", encoding="utf-8") as arquivo:
-        reader = csv.reader(arquivo)
-        next(reader, None)
-        
-        ids = []
-        
-        for row in reader:
-            if row and row[0].strip().isdigit():
-                ids.append(int(row[0]))
-        return (max(ids) + 1) if ids else 1
+    try: 
+        if not os.path.exists(nome_arquivo) or os.path.getsize(nome_arquivo) == 0:
+            return 1
+        with open (nome_arquivo, "r", encoding="utf-8") as arquivo:
+            reader = csv.reader(arquivo)
+            next(reader, None)
+            
+            ids = []
+            
+            for row in reader:
+                if row and row[0].strip().isdigit():
+                    ids.append(int(row[0]))
+            return (max(ids) + 1) if ids else 1
+    except Exception as error: 
+        print("Erro", error)
 
 def excluir_registros():
-    livros = "livros.csv"
-    usuarios = "usuarios.csv"
-    emprestimos = "emprestimos.csv"
+    try:
+        livros = "livros.csv"
+        usuarios = "usuarios.csv"
+        emprestimos = "emprestimos.csv"
 
-    if os.path.exists(livros):
-        os.remove(livros)
-    if os.path.exists(usuarios):
-        os.remove(usuarios)
-    if os.path.exists(emprestimos):
-        os.remove(emprestimos)
-    pass
+        if os.path.exists(livros):
+            os.remove(livros)
+        if os.path.exists(usuarios):
+            os.remove(usuarios)
+        if os.path.exists(emprestimos):
+            os.remove(emprestimos)
+    except Exception as error: 
+        print("Erro", error)
 
 def criar_cabecalho():
-    with open ("livros.csv", "w", newline="", encoding="utf-8") as arquivo:
-        writer = csv.writer(arquivo)
-        writer.writerow(["id", "titulo", "autor", "ano de lancamento", "isbn", "status"])
+    try: 
+        with open ("livros.csv", "w", newline="", encoding="utf-8") as arquivo:
+            writer = csv.writer(arquivo)
+            writer.writerow(["id", "titulo", "autor", "ano de lancamento", "isbn", "status"])
 
-    with open ("usuarios.csv", "w", newline="", encoding="utf-8") as arquivo:
-        writer = csv.writer(arquivo)
-        writer.writerow(["id", "nome", "matricula", "email"])
+        with open ("usuarios.csv", "w", newline="", encoding="utf-8") as arquivo:
+            writer = csv.writer(arquivo)
+            writer.writerow(["id", "nome", "matricula", "email"])
 
-    with open ("emprestimos.csv", "w", newline="", encoding="utf-8") as arquivo:
-        writer = csv.writer(arquivo)
-        writer.writerow(["id", "usuario_fk", "livro_fk", "data_retirada", "data_devolucao"])
-    pass
+        with open ("emprestimos.csv", "w", newline="", encoding="utf-8") as arquivo:
+            writer = csv.writer(arquivo)
+            writer.writerow(["id", "usuario_fk", "livro_fk", "data_retirada", "data_devolucao"])  
+    except Exception as error: 
+        print("Erro", error)
 
 def carregar_dados ():    
-    if os.path.exists("livros.csv"):
-            with open ("livros.csv", "r", encoding="utf-8") as arquivo:
-                reader = csv.DictReader(arquivo)
-                for linhas in arquivo:
-                    livros.append(linhas)
+    try:
+        if os.path.exists("livros.csv"):
+                with open ("livros.csv", "r", encoding="utf-8") as arquivo:
+                    reader = csv.DictReader(arquivo)
+                    for linhas in arquivo:
+                        livros.append(linhas)
 
-    if os.path.exists("usuarios.csv"):
-            with open ("usuarios.csv", "r", encoding="utf-8") as arquivo:
-                reader = csv.DictReader(arquivo)
-                for linhas in arquivo:
-                    usuarios.append(linhas)
+        if os.path.exists("usuarios.csv"):
+                with open ("usuarios.csv", "r", encoding="utf-8") as arquivo:
+                    reader = csv.DictReader(arquivo)
+                    for linhas in arquivo:
+                        usuarios.append(linhas)
 
-    if os.path.exists("emprestimos.csv"):
-            with open ("emprestimos.csv", "r", encoding="utf-8") as arquivo:
-                reader = csv.DictReader(arquivo)
-                for linhas in arquivo:
-                    emprestimos.append(linhas)
-    pass
+        if os.path.exists("emprestimos.csv"):
+                with open ("emprestimos.csv", "r", encoding="utf-8") as arquivo:
+                    reader = csv.DictReader(arquivo)
+                    for linhas in arquivo:
+                        emprestimos.append(linhas)
+    except Exception as error: 
+        print("Erro", error)
 
 def cadastrar_livro ():
-    id_livro = gerar_id("livros.csv")
-    titulo = input("digite aqui o titulo do livro:")
-    autor = input("digite aqui o autor do livro:")
-    ano = input("digite aqui o ano do lançamento do livro:")
-    isbn = input("digite aqui o ISBN do livro:")
-    status = True
+    try:
+        id_livro = gerar_id("livros.csv")
+        titulo = input("digite aqui o titulo do livro:")
+        autor = input("digite aqui o autor do livro:")
+        ano = input("digite aqui o ano do lançamento do livro:")
+        isbn = input("digite aqui o ISBN do livro:")
+        status = True
 
-    informations = [id_livro, titulo, autor, ano, isbn, status]
+        informations = [id_livro, titulo, autor, ano, isbn, status]
 
-    with open ("livros.csv", "a", newline="", encoding="utf-8") as arquivo:
-        writer = csv.writer(arquivo)
-        writer.writerow(informations)
-        carregar_dados()
+        with open ("livros.csv", "a", newline="", encoding="utf-8") as arquivo:
+            writer = csv.writer(arquivo)
+            writer.writerow(informations)
+            carregar_dados()
+    except Exception as error: 
+        print("Erro", error)
 
 def cadastrar_usuario ():
-    id_usuario = gerar_id("usuarios.csv")
-    nome = input("digite aqui seu nome:")
-    matricula = input("digite aqui sua matrícula:")
-    email = input("digite aqui o seu e-mail:")
+    try:
+        id_usuario = gerar_id("usuarios.csv")
+        nome = input("digite aqui seu nome:")
+        matricula = input("digite aqui sua matrícula:")
+        email = input("digite aqui o seu e-mail:")
 
-    informations = [id_usuario, nome, matricula, email]
+        informations = [id_usuario, nome, matricula, email]
 
-    with open ("usuarios.csv", "a", newline="", encoding="utf-8") as arquivo:
-        writer = csv.writer(arquivo)
-        writer.writerow(informations)
-        carregar_dados()
+        with open ("usuarios.csv", "a", newline="", encoding="utf-8") as arquivo:
+            writer = csv.writer(arquivo)
+            writer.writerow(informations)
+            carregar_dados()
+    except Exception as error: 
+        print("Erro", error)
 
 def registrar_devolucao (): 
-    livro_fk = int(input("digite aqui o ID do livro que você deseja devolver:"))
+    try:
+        livro_fk = int(input("digite aqui o ID do livro que você deseja devolver:"))
 
-    livros = []
-    find = False
+        livros = []
+        find = False
 
-    with open ("livros.csv", "r", newline="", encoding="utf-8") as arquivo: 
-        reader = csv.reader(arquivo)
-        for line in reader: 
-            if line[0] == str(livro_fk):
-                if line[-1] == "False":
-                    line[-1] = "True"
-                    find = True
-                else:
-                    print("livro já disponível")
-            livros.append(line)
-    
-    if not find:
-        print("livro não está registrado ou já está disponivel!")
-        return 
+        with open ("livros.csv", "r", newline="", encoding="utf-8") as arquivo: 
+            reader = csv.reader(arquivo)
+            for line in reader: 
+                if line[0] == str(livro_fk):
+                    if line[-1] == "False":
+                        line[-1] = "True"
+                        find = True
+                    else:
+                        print("livro já disponível")
+                livros.append(line)
+        
+        if not find:
+            print("livro não está registrado ou já está disponivel!")
+            return 
 
-    with open("livros.csv", "w", newline="", encoding="utf-8") as arquivo: 
-        writer = csv.writer(arquivo)
-        writer.writerows(livros)
+        with open("livros.csv", "w", newline="", encoding="utf-8") as arquivo: 
+            writer = csv.writer(arquivo)
+            writer.writerows(livros)
 
-    print("livro devolvido!")
+        print("livro devolvido!")
+    except Exception as error: 
+        print("Erro", error)
 
 def cadastrar_emprestimo():
-    id_emprestimo = gerar_id("emprestimos.csv")
-    usuario_fk = int(input("digite o ID do usuário que será emprestado:"))
-    livro_fk = int(input("digite o ID do livro que será emprestado:"))
-    data_atual = date.today()
-    data_futura = data_atual + timedelta(days=7)
+    try:
+        id_emprestimo = gerar_id("emprestimos.csv")
+        usuario_fk = int(input("digite o ID do usuário que será emprestado:"))
+        livro_fk = int(input("digite o ID do livro que será emprestado:"))
+        data_atual = date.today()
+        data_futura = data_atual + timedelta(days=7)
 
-    livros = []
-    disponivel = False
+        livros = []
+        disponivel = False
 
-    with open("livros.csv", "r", newline="", encoding="utf-8") as arquivo:
-        reader = csv.reader(arquivo)
-        for linha in reader:
-            if linha[0] == str(livro_fk):
-                if linha[-1] == "True":
-                    disponivel = True
-                    linha[-1] = "False"
-            livros.append(linha)
+        with open("livros.csv", "r", newline="", encoding="utf-8") as arquivo:
+            reader = csv.reader(arquivo)
+            for linha in reader:
+                if linha[0] == str(livro_fk):
+                    if linha[-1] == "True":
+                        disponivel = True
+                        linha[-1] = "False"
+                livros.append(linha)
 
-    if not disponivel:
-        print("este livro já está emprestado ou não existe dentro do registro de livros!")
-        return
+        if not disponivel:
+            print("este livro já está emprestado ou não existe dentro do registro de livros!")
+            return
 
-    with open("livros.csv", "w", newline="", encoding="utf-8") as arquivo:
-        writer = csv.writer(arquivo)
-        writer.writerows(livros)
+        with open("livros.csv", "w", newline="", encoding="utf-8") as arquivo:
+            writer = csv.writer(arquivo)
+            writer.writerows(livros)
 
-    informations = [id_emprestimo, usuario_fk, livro_fk, data_atual, data_futura]
+        informations = [id_emprestimo, usuario_fk, livro_fk, data_atual, data_futura]
 
-    with open("emprestimos.csv", "a", newline="", encoding="utf-8") as arquivo:
-        writer = csv.writer(arquivo)
-        writer.writerow(informations)
-        carregar_dados()
+        with open("emprestimos.csv", "a", newline="", encoding="utf-8") as arquivo:
+            writer = csv.writer(arquivo)
+            writer.writerow(informations)
+            carregar_dados()
 
-    print("empréstimo salvo com sucesso!")
-    print(f"data do emprestimo: {data_atual} | data da devolucao: {data_futura}")
+        print("empréstimo salvo com sucesso!")
+        print(f"data do emprestimo: {data_atual} | data da devolucao: {data_futura}")
+    except Exception as error: 
+        print("Erro", error)
 
 def listar_emprestimos():
-    with open ("emprestimos.csv", "r", encoding="utf-8") as arquivo:
-        reader = csv.reader(arquivo)
-        # next(arquivo, None)
+    try:
+        with open ("emprestimos.csv", "r", encoding="utf-8") as arquivo:
+            reader = csv.reader(arquivo)
+            # next(arquivo, None)
 
-        for lines in reader:
-            print(", ".join(lines))
+            for lines in reader:
+                print(", ".join(lines))
+    except Exception as error: 
+        print("Erro", error)
 
 def listar_livros_disponiveis(): 
-    livros_disponiveis = []
+    try:
+        livros_disponiveis = []
 
-    with open ("livros.csv", "r", newline="", encoding="utf-8") as arquivo: 
-        reader = csv.reader(arquivo)
-        for line in reader: 
-            if line[-1] == "True":
-                livros_disponiveis.append(line)
-            
-    if not livros_disponiveis:
-        print("nenhum livro disponível")
-        return
+        with open ("livros.csv", "r", newline="", encoding="utf-8") as arquivo: 
+            reader = csv.reader(arquivo)
+            for line in reader: 
+                if line[-1] == "True":
+                    livros_disponiveis.append(line)
+                
+        if not livros_disponiveis:
+            print("nenhum livro disponível")
+            return
 
-    print("livros disponíveis para empréstimos:")     
-    for livro in livros_disponiveis:
-        print(f"ID: {livro[0]} | Título: {livro[1]} | Autor: {livro[2]} | Ano: {livro[3]} | ISBN: {livro[4]}")   
+        print("livros disponíveis para empréstimos:")     
+        for livro in livros_disponiveis:
+            print(f"ID: {livro[0]} | Título: {livro[1]} | Autor: {livro[2]} | Ano: {livro[3]} | ISBN: {livro[4]}") 
+    except Exception as error: 
+        print("Erro", error)  
 
 def limpar_console () :
     os.system("cls")
